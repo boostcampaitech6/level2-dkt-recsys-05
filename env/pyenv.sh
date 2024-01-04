8f8a1e1e4523b9da2c3fe7031f959c6c43875d28
Kyung won <power16one5@naver.com>
2024-01-04 09:00:03 +0900

    bug: 필요한 패키지 추가 및 PATH등록 수정했습니다.

--- env/pyenv.sh
+++ env/pyenv.sh
@@ -1,12 +1,14 @@
 ### python 컴파일에 필요한 패키지들을 설치
-apt install curl libbz2-dev libssl-dev libsqlite3-dev liblzma-dev libffi-dev libncursesw5-dev libreadline-dev build-essential
-
-### pyenv 설치
+PACKAGES='curl libbz2-dev libssl-dev libsqlite3-dev liblzma-dev libffi-dev libncursesw5-dev libreadline-dev build-essential wget locale ilibgdbm-dev libnss3-dev zlib1g-dev'
+apt install $PACKAGES 
 curl https://pyenv.run | bash
 
+### PATH에 pyenv 등록
 echo 'export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"' >> ~/.bashrc
+
+source ~/.bashrc
 
 ### python 3.10.13 설치
 pyenv install 3.10.13

2b76ee59b5c9d2f76956f65f10c357cb5cc75e9f
nohkwanok <cloq@kakao.com>
2024-01-04 22:28:34 +0900

    remove all

--- base_code/.vscode-server/extensions/mechatroner.rainbow-csv-3.10.0/rbql_core/rbql/_version.py
+++
@@ -1,3 +0,0 @@
-# Explanation of this file purpose: https://stackoverflow.com/a/16084844/2898283
-__version__ = '0.27.0'
-
--- base_code/.vscode-server/extensions/ms-toolsai.vscode-jupyter-cell-tags-0.1.8/src/helper.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-import * as vscode from 'vscode';
-
-export function getCellTags(cell: vscode.NotebookCell) {
-	return cell.metadata.custom?.metadata?.tags ?? [];
-}

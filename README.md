e7ab5f276c1f2d1bbfb4007cfcbbd7f7f28168b3
seokulee <cd1004s@gmail.com>
2024-01-11 05:10:19 +0000

    fix: package manager, pyenv installer
    - update package manager before install pyenv
    - locale settings
    - pyenv.sh - PACKAGES : fix typo, del wget, del locale settings

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 # Environment Setting Guide
 
+서버 할당 후 패키지 관리자 update 및 locale 설정하기.
+
+```bash
+$ apt update
+$ apt-get update
+$ pip install --upgrade pip
+$ apt install locales
+$ locale-gen en_US.UTF-8
+$ update-locale LANG=en_US.UTF-8
+```
+
 할당받은 서버에서 pyenv.sh을 실행하면 pyenv가 설치됩니다.
 
-```
+```bash
 $ bash pyenv.sh
 $ source ~/.bashrc
 ```
 
 poetry를 설치하고 cache 디렉토리를 변경해주세요.
-```
+
+```bash
 $ poetry config cache-dir /data/ephemeral/.cache/pypoetry
 ```
--- pyenv.sh
+++ pyenv.sh
@@ -1,7 +1,6 @@
 ### python 컴파일에 필요한 패키지들을 설치
-PACKAGES='curl libbz2-dev libssl-dev libsqlite3-dev liblzma-dev libffi-dev libncursesw5-dev libreadline-dev build-essential wget locales libgdbm-dev libnss3-dev zlib1g-dev tk-dev'
-apt update
-apt install $PACKAGES -y
+PACKAGES='curl libbz2-dev libssl-dev libsqlite3-dev liblzma-dev libffi-dev libncursesw5-dev libreadline-dev build-essential libgdbm-dev libnss3-dev zlib1g-dev'
+apt install $PACKAGES
 curl https://pyenv.run | bash
 
 ### PATH에 pyenv 등록
@@ -10,10 +9,6 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"
 eval "$(pyenv init -)"' >> ~/.bashrc
 
 source ~/.bashrc
-
-### locale 설정
-locale-gen en_US.UTF-8
-update-locale LANG=en_US.UTF-8
 
 ### python 3.10.13 설치
 pyenv install 3.10.13

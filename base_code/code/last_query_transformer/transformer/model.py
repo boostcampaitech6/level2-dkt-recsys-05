505b3ac3f41310fc744f45b50a809de789669d0c
Kyung won <power16one5@naver.com>
2024-01-12 17:10:32 +0900

    wip

--- base_code/code/last_query_transformer/transformer/model.py
+++ base_code/code/last_query_transformer/transformer/model.py
@@ -1,10 +1,6 @@
 import torch
 import torch.nn as nn
 
-try:
-    from transformers.modeling_bert import BertConfig, BertEncoder, BertModel    
-except:
-    from transformers.models.bert.modeling_bert import BertConfig, BertEncoder, BertModel   
 
 class TransformerModel(nn.Module):
     def __init__(self, cfg):
@@ -82,3 +78,39 @@ class TransformerModel(nn.Module):
         pred_y = self.reg_layer(sequence_output)
 
         return pred_y
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, embed_size, heads):
+        super(MultiHeadAttention, self).__init__()
+        self.embed_size = embed_size
+        self.heads = heads
+        self.head_dim = embed_size // heads
+
+        assert (
+            self.head_dim * heads == embed_size
+        ), "Embedding size needs to be divisible by heads"
+
+        self.values = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.keys = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.queries = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.fc_out = nn.Linear(heads * self.head_dim, embed_size)
+
+    def forward(self, values, keys, query, mask):
+        N = query.shape[0]
+        value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
+
+        # Split the embedding into self.heads different pieces
+        values = values.reshape(N, value_len, self.heads, self.head_dim)
+        keys = keys.reshape(N, key_len, self.heads, self.head_dim)
+        queries = query.reshape(N, query_len, self.heads, self.head_dim)
+
+        values = self.values(values)
+        keys = self.keys(keys)
+        queries = self.queries(queries)
+
+        # Attention mechanism
+        # ...
+
+        out = self.fc_out(out)
+        return out

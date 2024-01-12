ad8521d8f5ff5efb71312fc04d10d7bb548ce0c4
seokulee <cd1004s@gmail.com>
2024-01-12 08:06:44 +0000

    fix: args -> yaml.config
    poetry add pyyaml
    main args->cfg

--- base_code/code/dkt/train.py
+++ base_code/code/dkt/train.py
@@ -5,7 +5,7 @@ import torch
 import wandb
 
 from dkt import trainer
-from dkt.args import parse_args
+from configs import load_config
 from dkt.dataloader import Preprocess
 from dkt.utils import get_logger, set_seeds, logging_conf
 
@@ -13,27 +13,26 @@ from dkt.utils import get_logger, set_seeds, logging_conf
 logger = get_logger(logging_conf)
 
 
-def main(args):
+def main(cfg):
     wandb.login()
-    wandb.init(config=vars(args))
-    set_seeds(args.seed)
-    args.device = "cuda" if torch.cuda.is_available() else "cpu"
-    
+    wandb.init(cfg['project'])
+    set_seeds(cfg['seed'])
+    cfg['device'] = "cuda" if torch.cuda.is_available() else "cpu"
+
     logger.info("Preparing data ...")
-    preprocess = Preprocess(args)
-    preprocess.load_train_data(file_name=args.file_name)
+    preprocess = Preprocess(cfg)
+    preprocess.load_train_data(file_name=cfg['file_name'])
     train_data: np.ndarray = preprocess.get_train_data()
     train_data, valid_data = preprocess.split_data(data=train_data)
-    wandb.init(project="dkt_jsj", config=vars(args))
-    
+
     logger.info("Building Model ...")
-    model: torch.nn.Module = trainer.get_model(args=args).to(args.device)
-    
+    model: torch.nn.Module = trainer.get_model(args=cfg).to(cfg['device'])
+
     logger.info("Start Training ...")
-    trainer.run(args=args, train_data=train_data, valid_data=valid_data, model=model)
+    trainer.run(args=cfg, train_data=train_data, valid_data=valid_data, model=model)
 
 
 if __name__ == "__main__":
-    args = parse_args()
-    os.makedirs(args.model_dir, exist_ok=True)
-    main(args)
+    cfg = load_config("config.yaml")
+    os.makedirs(cfg['model_dir'], exist_ok=True)
+    main(cfg)

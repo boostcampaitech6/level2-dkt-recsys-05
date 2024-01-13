bf52c00af07ba306e66d1adb9b57f2a346dc7dc2
Kyung won <power16one5@naver.com>
2024-01-13 12:59:17 +0900

    wip

--- base_code/code/last_query_transformer/transformer/model.py
+++ base_code/code/last_query_transformer/transformer/model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 
 class TransformerModel(nn.Module):
@@ -81,36 +82,44 @@ class TransformerModel(nn.Module):
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, embed_size, heads):
-        super(MultiHeadAttention, self).__init__()
-        self.embed_size = embed_size
-        self.heads = heads
-        self.head_dim = embed_size // heads
+    def __init__(self, cfg, USE_BIAS=True):
+        super(MultiHeadAttention,self).__init__()
+        if (cfg.d_feat % cfg.n_head) != 0:
+            raise ValueError("d_feat(%d) should be divisible by b_head(%d)"%(cfg.d_feat, cfg.nhead))
+        self.d_feat = cfg.d_feat
+        self.n_head = cfg.n_head
+        self.d_head = self.d_feat // self.n_head
+        self.sq_d_k = np.sqrt(self.d_head)
+        self.dropout = nn.Dropout(p=self.cfg.dropout)
 
-        assert (
-            self.head_dim * heads == embed_size
-        ), "Embedding size needs to be divisible by heads"
+        self.lin_Q = nn.Linear(self.d_feat, self.d_feat, USE_BIAS)  # the input dim needs to be changed to the size of seq_emb
+        self.lin_K = nn.Linear(self.d_feat, self.d_feat, USE_BIAS)
+        self.lin_V = nn.Linear(self.d_feat, self.d_feat, USE_BIAS)
+        self.lin_O = nn.Linear(self.d_feat, self.d_feat, USE_BIAS)
+        
 
-        self.values = nn.Linear(self.head_dim, self.head_dim, bias=False)
-        self.keys = nn.Linear(self.head_dim, self.head_dim, bias=False)
-        self.queries = nn.Linear(self.head_dim, self.head_dim, bias=False)
-        self.fc_out = nn.Linear(heads * self.head_dim, embed_size)
+    def forward(self, input, mask):
+        n_batch = input.shape[0]
+        
+        Q = self.lin_Q(input)
+        K = self.lin_K(input)
+        V = self.lin_V(input)
 
-    def forward(self, values, keys, query, mask):
-        N = query.shape[0]
-        value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
+        Q = Q.view(n_batch, -1, self.n_head, self.d_head)
+        K = K.view(n_batch, -1, self.n_head, self.d_head)
+        V = V.view(n_batch, -1, self.n_head, self.d_head)
 
-        # Split the embedding into self.heads different pieces
-        values = values.reshape(N, value_len, self.heads, self.head_dim)
-        keys = keys.reshape(N, key_len, self.heads, self.head_dim)
-        queries = query.reshape(N, query_len, self.heads, self.head_dim)
+        Q = Q.transpose(1, 2)
+        K = K.transpose(1, 2)
+        V = V.transpose(1, 2)
+ 
+        scores = torch.matmul(Q, K.transpose(-1, -2)) / self.sq_d_k 
+        scores = scores.masked_fill(mask == 0, -float('inf'))
+        attention = torch.softmax(scores, dim=-1)
+        output = torch.matmul(self.dropout(attention), V) 
 
-        values = self.values(values)
-        keys = self.keys(keys)
-        queries = self.queries(queries)
+        output = output.transpose(1, 2).contiguous()
+        output = output.view(n_batch, -1, self.d_feat)
+        output = self.lin_O(output)
 
-        # Attention mechanism
-        # ...
-
-        out = self.fc_out(out)
-        return out
+        return output

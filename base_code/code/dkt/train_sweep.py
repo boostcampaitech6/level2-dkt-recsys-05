80a3e49e84f40604a768fcbf17b21d3faa0868a8
ksj1368 <ksj006151@gmail.com>
2024-01-14 01:12:03 +0000

    refactor: yaml로 프로젝트 명 받아오기 + args로 yaml 경로 받아오기

--- base_code/code/dkt/train_sweep.py
+++ base_code/code/dkt/train_sweep.py
@@ -13,7 +13,7 @@ import yaml
 logger = get_logger(logging_conf)
 
 def main(args, train_data, valid_data):
-    wandb.init(project="sweep_test", config=vars(args))
+    wandb.init(project=sweep_configuration["project"], config=vars(args))
     
     logger.info("Building Model ...")
     model: torch.nn.Module = trainer.get_model(args=args).to(args.device)
@@ -29,23 +29,11 @@ if __name__ == "__main__":
     os.makedirs(args.model_dir, exist_ok=True)
     
     wandb.login()
-    yaml_file = '/opt/ml/code/sweep_yaml/base_model.yaml'
-    '''
-    if args.model.lower() == "lstm":
-        yaml_file = '/opt/ml/code/sweep_yaml/base_model.yaml'
-    elif args.model.lower() == "lstmattn":
-        yaml_file = '/opt/ml/code/sweep_yaml/lstmattn.yaml'
-    elif args.model.lower() == "gru":
-        yaml_file = '/opt/ml/code/sweep_yaml/gru.yaml'
-    elif args.model.lower() == "gruattn":
-        yaml_file = '/opt/ml/code/sweep_yaml/gruattn.yaml'
-    elif args.model.lower() == 'bert':
-        yaml_file = '/opt/ml/code/sweep_yaml/bert.yaml'
-    '''
+    yaml_file = args.yaml_dir + args.yaml
     
     with open(yaml_file) as f:
         sweep_configuration = yaml.load(f, Loader=yaml.FullLoader)
-    sweep_id = wandb.sweep(sweep=sweep_configuration, project='sweep_test')
+    sweep_id = wandb.sweep(sweep=sweep_configuration, project=sweep_configuration["project"])
    
     logger.info("Preparing data ...")
     preprocess = Preprocess(args)

01a7c907daa22b468131b27f1354a850b0d2f4f1
ksj1368 <ksj006151@gmail.com>
2024-01-15 09:52:52 +0000

    fix: args -> cfg

--- base_code/code/dkt/dkt/trainer_sweep.py
+++ base_code/code/dkt/dkt/trainer_sweep.py
@@ -12,43 +12,43 @@ from .trainer import train, validate
 
 logger = get_logger(logger_conf=logging_conf)
 
-def run(args,
+def run(cfg,
         train_data: np.ndarray,
         valid_data: np.ndarray,
         model: nn.Module):
-    train_loader, valid_loader = get_loaders(args=args, train=train_data, valid=valid_data)
+    train_loader, valid_loader = get_loaders(cfg=cfg, train=train_data, valid=valid_data)
 
     # For warmup scheduler which uses step interval
-    args.total_steps = int(math.ceil(len(train_loader.dataset) / args.batch_size)) * (
-        args.n_epochs
+    cfg['total_steps'] = int(math.ceil(len(train_loader.dataset) / cfg['batch_size'])) * (
+        cfg['n_epochs']
     )
-    args.warmup_steps = args.total_steps // 10
+    cfg['warmup_steps'] = cfg['total_steps'] // 10
 
-    optimizer = get_optimizer(model=model, args=args)
-    scheduler = get_scheduler(optimizer=optimizer, args=args)
+    optimizer = get_optimizer(model=model, cfg=cfg)
+    scheduler = get_scheduler(optimizer=optimizer, cfg=cfg)
 
     best_auc = -1
     early_stopping_counter = 0
-    for epoch in range(args.n_epochs):
+    for epoch in range(cfg['n_epochs']):
         logger.info("Start Training: Epoch %s", epoch + 1)
 
         # TRAIN
         train_auc, train_acc, train_loss = train(train_loader=train_loader,
                                                  model=model, optimizer=optimizer,
-                                                 scheduler=scheduler, args=args)
+                                                 scheduler=scheduler, cfg=cfg)
 
         # VALID
-        auc, acc = validate(valid_loader=valid_loader, model=model, args=args)
+        auc, acc = validate(valid_loader=valid_loader, model=model, cfg=cfg)
 
         if auc > best_auc:
             best_auc = auc
             early_stopping_counter = 0
         else:
             early_stopping_counter += 1
-            if early_stopping_counter >= args.patience:
+            if early_stopping_counter >= cfg['patience']:
                 logger.info(
                     "EarlyStopping counter: %s out of %s",
-                    early_stopping_counter, args.patience
+                    early_stopping_counter, cfg['patience']
                 )
                 break
         wandb.log(dict(epoch=epoch,
@@ -59,5 +59,5 @@ def run(args,
                        valid_acc_epoch=acc,
                        best_auc = best_auc))        
         # scheduler
-        if args.scheduler == "plateau":
+        if cfg["scheduler"] == "plateau":
             scheduler.step(best_auc)

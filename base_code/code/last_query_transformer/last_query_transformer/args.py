39ec3625c29a133f67386f7c5e3d1a7565c79954
Kyung won <power16one5@naver.com>
2024-01-18 15:55:50 +0900

    feat: sweep 생성

--- base_code/code/last_query_transformer/last_query_transformer/args.py
+++ base_code/code/last_query_transformer/last_query_transformer/args.py
@@ -6,23 +6,20 @@ def parse_args():
 
     parser.add_argument("--seed", type=int, help="seed")
     parser.add_argument("--use_cuda_if_available", type=bool, help="Use GPU")
-    
     parser.add_argument("--data_dir", type=str, help="")
-    
     parser.add_argument("--output_dir", type=str, help="")
-    
-    parser.add_argument("--batch_size", type=int, help="")
-    parser.add_argument("--emb_dim", type=int, help="")
-    parser.add_argument("--hidden_dim", type=int, help="")
-    parser.add_argument("--n_layers", type=int, help="")
-    parser.add_argument("--n_heads", type=int, help="")
-
-    parser.add_argument("--seq_len", type=int, help="")
-    
-    parser.add_argument("--n_epochs", type=int, help="")
-    parser.add_argument("--lr", type=float, help="")
     parser.add_argument("--model_dir", type=str, help="")
     parser.add_argument("--model_name", type=str, help="")
+    
+    parser.add_argument("--batch_size", type=int, help="")
+    parser.add_argument("--emb_size", type=int, help="")
+    parser.add_argument("--hidden_size", type=int, help="")
+    parser.add_argument("--n_layers", type=int, help="")
+    parser.add_argument("--n_head", type=int, help="")
+    parser.add_argument("--seq_len", type=int, help="")
+    parser.add_argument("--n_epochs", type=int, help="")
+    parser.add_argument("--lr", type=float, help="")
+    parser.add_argument("--dropout", type=float, help="")
 
     parser.add_argument("--verbose", type=bool, help="")
     

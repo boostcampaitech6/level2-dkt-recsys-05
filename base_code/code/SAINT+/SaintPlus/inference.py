fix : Scaling 기능 추가

base_code/code/SAINT+/SaintPlus/inference.py
@@ -43,13 +43,14 @@ def inference(cfg) :
     submission = []
     for step, data in enumerate(test_loader) :
         item_id         = data[0].to(device).long()
+    #   test_id         = data[1].to(device).long()
         lag_time        = data[1].to(device).float()
         elapsed_time    = data[2].to(device).float()
         item_acc        = data[3].to(device).float()
         user_acc        = data[4].to(device).float()
         answer_correct  = data[5].to(device).long()
         
-        preds = model(item_id, lag_time, elapsed_time, item_acc, user_acc, answer_correct)
+        preds = model(item_id, lag_time, elapsed_time, item_acc, user_acc, answer_correct) # test_id
         
         pred = preds[:, -1]
         submission.extend(pred.data.cpu().numpy())
@@ -58,7 +59,7 @@ def inference(cfg) :
     return submission
 
 if __name__ == '__main__' :
-    cfg = load_config('./config.yaml')
+    cfg = load_config('../config.yaml')
     submission = inference(cfg)
     submit = pd.DataFrame({'id' : np.arange(len(submission)),
                            'prediction' : submission})

base_code/code/SAINT+/SaintPlus/model.py
@@ -37,7 +37,6 @@ class SaintPlus(nn.Module) :
         self.elapT_emb = nn.Linear(1, d_model, bias = False)
         self.itemAcc_emb = nn.Linear(1, d_model, bias = False)
         self.userAcc_emb = nn.Linear(1, d_model, bias = False)
-
         self.answerCorr_emb = nn.Embedding(3, d_model)
 
         self.emb_dense1 = nn.Linear(2 * d_model, d_model)
@@ -52,12 +51,13 @@ class SaintPlus(nn.Module) :
         self.FFN = FFN(d_model, d_ffn, dropout = dropout)
         self.final_layer = nn.Linear(d_model, 1)
 
-    def forward(self, item_id, lag_time, elapsed_time, item_avg, user_avg, answer_correct) :
+    def forward(self, item_id, lag_time, elapsed_time, item_acc, user_acc, answer_correct) : # test_id
+        
         device = item_id.device
         seq_len = item_id.shape[1]
 
-        item_emb = self.item_emb(item_id)
-        # test_emb = self.test_emb(test_id)
+        itemid_emb = self.item_emb(item_id)
+    #   testid_emb = self.test_emb(test_id)
 
         lag_time = torch.log(lag_time + 1)
         lag_time = lag_time.view(-1, 1)                             # [batch * seq_len, 1]
@@ -69,21 +69,21 @@ class SaintPlus(nn.Module) :
         elapsed_time = self.elapT_emb(elapsed_time)                 # [batch * seq_len, d_model]
         elapsed_time = elapsed_time.view(-1, seq_len, self.d_model) # [batch,  seq_len, d_model]
         
-        item_avg = torch.log(item_avg + 1)
-        item_avg = item_avg.view(-1, 1)                             # [batch * seq_len, 1]
-        item_avg = self.itemAcc_emb(item_avg)                       # [batch * seq_len, d_model]
-        item_avg = item_avg.view(-1, seq_len, self.d_model)         # [batch,  seq_len, d_model]
+        item_acc = torch.log(item_acc + 1)
+        item_acc = item_acc.view(-1, 1)                             # [batch * seq_len, 1]
+        item_acc = self.itemAcc_emb(item_acc)                       # [batch * seq_len, d_model]
+        item_acc = item_acc.view(-1, seq_len, self.d_model)         # [batch,  seq_len, d_model]
         
-        user_avg = torch.log(user_avg + 1)
-        user_avg = user_avg.view(-1, 1)                             # [batch * seq_len, 1]
-        user_avg = self.userAcc_emb(user_avg)                       # [batch * seq_len, d_model]
-        user_avg = user_avg.view(-1, seq_len, self.d_model)         # [batch,  seq_len, d_model]
+        user_acc = torch.log(user_acc + 1)
+        user_acc = user_acc.view(-1, 1)                             # [batch * seq_len, 1]
+        user_acc = self.userAcc_emb(user_acc)                       # [batch * seq_len, d_model]
+        user_acc = user_acc.view(-1, seq_len, self.d_model)         # [batch,  seq_len, d_model]
         
         answer_correct_emb = self.answerCorr_emb(answer_correct)
 
-        encoder_val = torch.cat((item_emb, lag_time), axis = -1)
+        encoder_val = torch.cat((itemid_emb, lag_time), axis = -1)
         encoder_val = self.emb_dense1(encoder_val)
-        decoder_val = torch.cat((lag_time, elapsed_time, item_avg, user_avg, answer_correct_emb), axis = -1)
+        decoder_val = torch.cat((lag_time, elapsed_time, item_acc, user_acc, answer_correct_emb), axis = -1)
         decoder_val = self.emb_dense2(decoder_val)
 
         pos = torch.arange(seq_len).unsqueeze(0).to(device)

base_code/code/SAINT+/SaintPlus/preprocess.py
@@ -1,38 +1,30 @@
 import numpy as np
 import pandas as pd
-import time
+from sklearn.preprocessing import RobustScaler
 import pickle
-from datetime import datetime
-from collections import defaultdict
 from .utils import load_config
 
-# 개인별 문제 푸는데 걸린 시간
-def Lag_Time(df) :
+# 개인별 테스트를 푸는데 걸린 시간
+def LagTime(df) :
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'])
     time_dict = {}
     lag_time = np.zeros(len(df), dtype = np.float32)
-    for idx, col in enumerate(df[['userID', 'Timestamp', 'testId']].values) :
-        col[1] = time.mktime(datetime.strptime(col[1],'%Y-%m-%d %H:%M:%S').timetuple())
-        
-        # 처음 문제 푸는 유저
-        if col[0] not in time_dict :
-            lag_time[idx] = 0
-            time_dict[col[0]] = [col[1], col[2], 0] # last_timestamp, last_testID, last_LagTime
-        
-        else :
-            
-            # 이 시험지를 풀어봤다면
-            if col[2] == time_dict[col[0]][1] :
-                lag_time[idx] = time_dict[col[0]][2]
-            
-            # 이 시험지를 푼 적 없다면
-            else :
-                lag_time[idx] = col[1] - time_dict[col[0]][0]
-                time_dict[col[0]][0] = col[1]
-                time_dict[col[0]][1] = col[2]
-                time_dict[col[0]][2] = lag_time[idx]
+    for idx, (user, time, test) in enumerate(df[['userID', 'Timestamp', 'testId']].values) :
 
-    df['lag_time'] = lag_time/1000/60 # 분으로 출력
-    df['lag_time'] = df['lag_time'].clip(0, 1440) # 1440분(하루) 이상이면 1440으로 변환
+        if user not in time_dict :
+            lag_time[idx] = 0
+            time_dict[user] = [time, test, 0]
+
+        else :
+            if test == time_dict[user][1] :
+                lag_time[idx] = time_dict[user][2]
+            else :
+                lag_time[idx] = (time - time_dict[user][0]).total_seconds()
+                time_dict[user][0] = time
+                time_dict[user][1] = test
+                time_dict[user][2] = lag_time[idx]
+    df['lag_time'] = lag_time / 1000 / 60 # 분 단위로 변환
+    df['lag_time'] = df['lag_time'].clip(0, 1440)
     return df['lag_time']
 
 # 문제 푸는데 걸린 시간
@@ -41,7 +33,7 @@ def ElapsedTime(df) :
     df['Timestamp'] = pd.to_datetime(df['Timestamp'])
     df['elapsed_time'] = df.groupby(['userID', 'testId'], as_index = False)['Timestamp'].diff().shift(0).dt.total_seconds()
     df['elapsed_time'] = df['elapsed_time'].fillna(0)
-    df['elapsed_time'] = np.where(df['elapsed_time'] > 3600, np.nan, df['elapsed_time']) # 1시간 이상 결측치 처리
+    df['elapsed_time'] = np.where(df['elapsed_time'] > 1200, np.nan, df['elapsed_time'])
     
     df['problemID'] = df['assessmentItemID'].apply(lambda x : x[7:]).astype('int8')
     df['elapsed_time'] = df.groupby('problemID')['elapsed_time'].transform(lambda x : x.fillna(x.mean()))
@@ -66,76 +58,82 @@ def User_Acc(df) :
 
 # 범주형 변수 인덱싱
 def indexing(df, col) :
-    col2idx = {v : (k+1) for k, v in enumerate(sorted(df[col].unique()))}
+    col2idx = {v : (k+1) for k, v in enumerate(df[col].unique())}
     df[col] = df[col].map(col2idx)
-    return df
+    return df[col]
 
 def Feature_Engineering(df) :
-    print('Start Feature Engineering')
+    df = df.sort_values(by = ['userID', 'Timestamp']).reset_index(drop = True)
     df.index = df.index.astype('uint32')
 
-    df['lag_time'] = Lag_Time(df)
+    # Feature 생성
+    df['lag_time'] = LagTime(df)
     df['elapsed_time'] = ElapsedTime(df)
     df['item_acc'] = Item_Acc(df)
     df['user_acc'] = User_Acc(df)
     
-    df = indexing(df, 'assessmentItemID')
-    df = indexing(df, 'testId')
-    
+    # Indexing
+    df['assessmentItemID'] = indexing(df, 'assessmentItemID')
+    df['testId'] = indexing(df, 'testId')
     df['answerCode'] += 1 # Wrong : 1 / Correct : 2
-    print('Finish Feature Engineering')
     return df
 
 def grouping(cfg, df, features, save_name) :
-    df_group = df[features].groupby('userID').apply(lambda df : (
-            df['assessmentItemID'].values,
-            df['testId'].values,
-            df['lag_time'].values,
-            df['elapsed_time'].values, 
-            df['item_acc'].values,
-            df['user_acc'].values,
-            df['answerCode'].values
+    print('User 수 : ', df['userID'].nunique())
+    df_group = df[features].groupby('userID').apply(lambda r : (
+            r['assessmentItemID'].values,
+            r['testId'].values,
+            r['lag_time'].values,
+            r['elapsed_time'].values, 
+            r['item_acc'].values,
+            r['user_acc'].values,
+            r['answerCode'].values
         ))
     
     with open(cfg['data_dir'] + f'{save_name}.pkl.zip', 'wb') as p :
         pickle.dump(df_group, p)
 
-def Preprocess(cfg, df, is_train = True) :
+def Preprocess(cfg, total, test, scaling = False) :
     print('Start Preprocess')
-    df = Feature_Engineering(df)
+    total = Feature_Engineering(total)
+    test  = Feature_Engineering(test)
     features = ['userID', 'assessmentItemID', 'testId', 'lag_time', 'elapsed_time', 'item_acc', 'user_acc', 'answerCode']
     
-    # Train / Valid Data
-    if is_train :
-        # valid_indices = set(df[df['answerCode'] != -1].index).intersection(set(df.reset_index().groupby('userID', as_index = False).last().set_index('index').index))
-        # train_df = df.loc[~df.index.isin(valid_indices)]
-        # valid_df = df.loc[df.index.isin(valid_indices)]
-        
-        valid_size = 0.99
-        train_df = df[:int(df.shape[0] * valid_size)]
-        valid_df = df[int(df.shape[0] * valid_size):]
-        print(f'Train : {train_df.shape}, Valid : {valid_df.shape}')
-        print('=' * 50)
+    if scaling :
+        time_col = ['lag_time', 'elapsed_time']
+        total_time, test_time = total[time_col], test[time_col]
 
-        print('Start Train and Valid Data Grouping')
-        grouping(cfg, train_df, features, 'Train_SP')
-        grouping(cfg, valid_df, features, 'Valid_SP')
-        print('Finish Preprocess')
+        rb_scaler = RobustScaler()
+        total_time_scaled = pd.DataFrame(rb_scaler.fit_transform(total_time), columns = time_col)
+        test_time_scaled = pd.DataFrame(rb_scaler.transform(test_time), columns = time_col)
+
+        total_df = pd.concat([total.drop(columns = time_col), total_time_scaled], axis = 1)
+        test_df = pd.concat([test.drop(columns = time_col), test_time_scaled], axis = 1)
     
-    # Test Data
-    else :
-        print('Start Test Data Grouping')
-        grouping(cfg, df, features, 'Test_SP')
-        print('Finish Preprocess')
+    else : 
+        total_df = total.copy()
+        test_df = test.copy()
+    
+    # valid_indices = set(df[df['answerCode'] != -1].index).intersection(set(df.reset_index().groupby('userID', as_index = False).last().set_index('index').index))
+    # train_df = df.loc[~df.index.isin(valid_indices)]
+    # valid_df = df.loc[df.index.isin(valid_indices)]
+    
+    print(total_df.shape, total.shape)
+    valid_size = 0.98
+    train_df = total_df[:int(total_df.shape[0] * valid_size)]
+    valid_df = total_df[int(total_df.shape[0] * valid_size):]
+    print(f'Train : {train_df[features].shape}, Valid : {valid_df[features].shape}')
+    print('=' * 50)
+
+    grouping(cfg, train_df, features, 'Train_SP')
+    grouping(cfg, valid_df, features, 'Valid_SP')
+    grouping(cfg, test_df,  features, 'Test_SP')
+    print('Finish Preprocess')
 
 
 if __name__ == '__main__' :
-    cfg = load_config('./config.yaml')
-    
-    total_data = cfg['data_dir'] + cfg['total_data_name']
-    total_df = pd.read_csv(total_data)
-    Preprocess(cfg, total_df, True)
-    
-    test_data = cfg['data_dir'] + cfg['test_data_name']
-    test_df = pd.read_csv(test_data)
-    Preprocess(cfg, test_df, False)
+    cfg = load_config('../config.yaml')
+
+    total_df = pd.read_csv(cfg['data_dir'] + cfg['total_data_name'])
+    test_df = pd.read_csv(cfg['data_dir'] + cfg['test_data_name'])
+    Preprocess(cfg, total_df, test_df, cfg['scale'])

base_code/code/SAINT+/SaintPlus/train.py
@@ -21,7 +21,7 @@ def save_model(cfg, model) :
 
 def train(cfg) :
     
-    wandb.run.name = cfg['runname'] + str(datetime.datetime.now().strftime('%y%m%d_%H%M%S'))
+    wandb.run.name = cfg['runname'] + '_' + str(datetime.datetime.now().strftime('%y%m%d_%H%M%S'))
     wandb.run.save()
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -34,7 +34,6 @@ def train(cfg) :
     seq_len = cfg['seq_len']
     n_question = cfg['n_question']
     n_test = cfg['n_test']
-    n_answer = cfg['n_answer']
 
     dropout = cfg['dropout']
     n_epochs = cfg['n_epochs']
@@ -78,7 +77,7 @@ def train(cfg) :
     best_auc = 0
     count = 0
     for epoch in range(n_epochs) :
-        print(f'============ Epoch {epoch + 1} Training ============')
+        print(f'============ Epoch {epoch + 1} Training   ============')
         model.train()
         t_s = time.time()
         train_loss = []
@@ -87,6 +86,7 @@ def train(cfg) :
         
         for step, data in enumerate(train_loader) :
             item_id         = data[0].to(device).long()
+        #   test_id         = data[1].to(device).long()
             lag_time        = data[1].to(device).float()
             elapsed_time    = data[2].to(device).float()
             item_acc        = data[3].to(device).float()
@@ -96,7 +96,7 @@ def train(cfg) :
 
             opt.optimizer.zero_grad()
             
-            preds = model(item_id, lag_time, elapsed_time, item_acc, user_acc, answer_correct)
+            preds = model(item_id, lag_time, elapsed_time, item_acc, user_acc, answer_correct) # test_id
             loss_mask = (answer_correct != 0)
             preds_masked = torch.masked_select(preds, loss_mask)
             label_masked = torch.masked_select(label, loss_mask)
@@ -120,6 +120,7 @@ def train(cfg) :
 
         for step, data in enumerate(valid_loader) :
             item_id         = data[0].to(device).long()
+        #   test_id         = data[1].to(device).long()
             lag_time        = data[1].to(device).float()
             elapsed_time    = data[2].to(device).float()
             item_acc        = data[3].to(device).float()
@@ -127,7 +128,7 @@ def train(cfg) :
             answer_correct  = data[5].to(device).long()
             label           = data[6].to(device).float()
             
-            preds = model(item_id, lag_time, elapsed_time, item_acc, user_acc, answer_correct)
+            preds = model(item_id, lag_time, elapsed_time, item_acc, user_acc, answer_correct) # test_id
             loss_mask = (answer_correct != 0)
             preds_masked = torch.masked_select(preds, loss_mask)
             label_masked = torch.masked_select(label, loss_mask)
@@ -173,6 +174,6 @@ def train(cfg) :
     return best_auc
 
 if __name__== '__main__' :
-    cfg = load_config('./config.yaml')
+    cfg = load_config('../config.yaml')
     seed_everything(42)
     train(cfg)